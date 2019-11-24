9aba99589b8a7d558964beca41e9d3152a62ce13
Ziyu Wang <17210720158@fudan.edu.cn>
2019-11-24 18:10:53 +0800

    first time push

---
+++ NewJourneyOfCPlusPlus.git/NewJourneyOfCPlusPlus.git/main.cpp
@@ -0,0 +1,19 @@
+//
+//  main.cpp
+//  NewJourneyOfCPlusPlus.git
+//
+//  Created by 王子昱 on 2019/11/24.
+//  Copyright © 2019 王子昱. All rights reserved.
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    cout << "I have finish my thesis, and will start learning c++ from scrach again" << endl;
+    
+    return 0;
+}
