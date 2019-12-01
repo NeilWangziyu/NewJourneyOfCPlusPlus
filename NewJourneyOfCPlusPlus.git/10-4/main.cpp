102b372ef8dee4de5bc87433d976baadcff1b7be
Ziyu Wang <17210720158@fudan.edu.cn>
2019-12-01 16:06:21 +0800

    366

--- NewJourneyOfCPlusPlus.git/10-4/main.cpp
+++ NewJourneyOfCPlusPlus.git/10-4/main.cpp
@@ -11,6 +11,8 @@
 #include <string>
 #include <list>
 #include <forward_list>
+#include <numeric>
+
 using namespace std;
 
 int main(int argc, const char * argv[]) {
@@ -29,6 +31,43 @@ int main(int argc, const char * argv[]) {
         cout << item << " ";
     }
     cout << endl;
+    
+//    use the istream to read input stream and store in vector
+//    istream_iterator<int> in_iter(cin);
+//    istream_iterator<int> eof;
+//    vector<int> vec;
+//    while (in_iter != eof) {
+//        vec.push_back(*in_iter++);
+//    }
+//    for (auto item:vec) {
+//        cout << item << " ";
+//    }
+//    等价：
+//    istream_iterator<int> in_iter2(cin), eof2;
+//    vector<int> vec2(in_iter2, eof2);
+    
+    cout << endl;
+    cout << "input some number, end with \\0:" << endl;
+    istream_iterator<int> in(cin), eof;
+    
+    cout << "sum is:" << accumulate(in, eof, 0) << endl;
+    
+    ostream_iterator<int> out_iter(cout, " ");
+    for (auto e : lst3) {
+        *out_iter ++ = e;
+    }
+    cout << endl;
+    
+    copy(lst2.begin(), lst2.end(), out_iter);
+    cout << endl;
+    
+    
+//    泛型算法结构：
+//    1. input iterator
+//    2. output iterator
+//    3. forward
+//    4. bidirectional
+//    5. random-access
     
     return 0;
 }
