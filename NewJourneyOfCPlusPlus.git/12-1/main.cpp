4e6ddfdb6612f1b03e12a68269261cd6a0403377
Ziyu Wang <17210720158@fudan.edu.cn>
2019-12-03 14:44:46 +0800

    429

--- NewJourneyOfCPlusPlus.git/12-1/main.cpp
+++ NewJourneyOfCPlusPlus.git/12-1/main.cpp
@@ -14,8 +14,11 @@
 
 using namespace std;
 
+class strBlobPtr;
+
 class strBlob
 {
+    friend class strBlobPtr;
 public:
     typedef std::vector<string>::size_type size_type;
     strBlob();
@@ -27,6 +30,9 @@ public:
     
     string & front();
     string & back();
+    
+    strBlobPtr begin();
+    strBlobPtr end();
     
     
 private:
@@ -62,6 +68,55 @@ void strBlob::pop_back()
     data->pop_back();
 }
 
+
+class strBlobPtr{
+public:
+    strBlobPtr():curr(0) {}
+    strBlobPtr(strBlob &a, size_t sz = 0):
+    wptr(a.data), curr(sz) {}
+    string & deref() const;
+    strBlobPtr& incr();
+
+    
+private:
+    shared_ptr<vector<string>> check(size_t, const string&) const;
+    weak_ptr<vector<string>> wptr;
+    size_t curr;
+};
+
+shared_ptr<vector<string>> strBlobPtr::check(size_t i, const string &msg) const
+{
+    auto ret = wptr.lock();
+    if (!ret) {
+        throw std::runtime_error("unbound strblobptr");
+    }
+    if (i >= ret->size()) {
+        throw std::out_of_range(msg);
+    }
+    return ret;
+//    return the pointer to vector
+}
+
+string& strBlobPtr::deref() const
+{
+    auto p = check(curr, "dereference past end");
+    return (*p)[curr];
+//    (*p) is pointer to vector
+}
+
+strBlobPtr& strBlobPtr::incr()
+{
+    check(curr, "increment past end of strblobptr");
+    ++curr;
+    return *this;
+}
+
+strBlobPtr strBlob::begin(){ return strBlobPtr(*this);}
+
+strBlobPtr strBlob::end() {
+    auto ret = strBlobPtr(*this, data->size());
+    return ret;
+}
 
 int main(int argc, const char * argv[]) {
     // insert code here...
@@ -118,6 +173,86 @@ int main(int argc, const char * argv[]) {
     cout << *up4 << endl;
     
 //    weakptr
+    auto oldp =  make_shared<int>(42);
+    weak_ptr<int> wp(oldp);
+    cout << wp.use_count() << endl;
     
+    
+    strBlobPtr ourbloptr(ourblo);
+    
+    cout << ourbloptr.deref() << endl;
+    ourbloptr.incr();
+    cout << ourbloptr.deref() << endl;
+    ourbloptr.incr();
+    cout << ourbloptr.deref() << endl;
+    
+    int *pia = new int[10];
+    
+    typedef int arrT[42];
+    int *parrT = new arrT;
+    
+    int *pia2 = new int[10]();
+    
+    string *psa = new string[10];
+    string *psa2 = new string[10] ();
+    
+    int *pia3 = new int[10]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    string *ps3 = new string[2]{"hi", "hihao"};
+    
+    delete [] pia;
+    delete [] parrT;
+    delete [] ps3;
+//    in delete动态数组, the [] is necessary
+    
+    unique_ptr<int []> up(new int[10]);
+    for(size_t i = 0; i != 10; ++i)
+    {
+        up[i] = i;
+    }
+
+    up.release();
+//    use the unique_ptr to manage 动态数组
+    
+//    share_ptr 不直接支持管理动态数组
+//    为了使用share_ptr, 必须提供删除器
+    shared_ptr<int> sp(new int[10], [](int *p) { delete [] p;});
+    sp.reset();
+    
+//    allocator: 帮助将内存分配和对象构造分离
+    allocator<string> alloc;
+    size_t n = 10;
+    auto const pa = alloc.allocate(n);
+    
+    auto qa = pa;
+    alloc.construct(qa++);
+    alloc.construct(qa++, 10, 'a');
+    alloc.construct(qa++, "hi, neil");
+//
+//    while (pa != qa) {
+//        cout << *pa << " ";
+//        pa ++;
+//    }
+//    cout << endl;
+    cout << *pa << endl;
+//    cout << *pa << endl;
+    while (qa != pa) {
+        alloc.destroy(--qa);
+    }
+    
+    
+    alloc.deallocate(pa, n);
+    
+    
+    allocator<int> alloc2;
+
+    vector<int> tesrtv {1,2,3,4,5};
+    auto ptest = alloc2.allocate(tesrtv.size() * 2);
+    auto ptest2 = uninitialized_copy(tesrtv.begin(), tesrtv.end(), ptest);
+    
+    uninitialized_fill_n(ptest2, tesrtv.size(), 42);
+    
+    cout << *ptest2 << endl;
+//    page 429, but still some problems
     return 0;
 }
+
